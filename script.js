fcbbb99ba04b33a5422dde04a5befbef2e8e951a
yashprojects001 <yash.pathrikar21@gmail.com>
2026-03-15 23:14:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function cosmic(){
+document.body.style.background="radial-gradient(circle,#000010,#000)";
+}
+
+function cyber(){
+document.body.style.background="linear-gradient(45deg,#0f0c29,#302b63,#24243e)";
+}
+
+function nature(){
+document.body.style.background="linear-gradient(#001a00,#003300)";
+}
+
+function abstractWorld(){
+document.body.style.background="linear-gradient(45deg,#ff512f,#dd2476)";
+}
